36db53536026423de4664d13d24f93e4865e6681
FabianCristancho <fabiancrisrincon@outlook.com>
2021-05-01 12:57:48 -0500

    verify length sudoku

--- sudoku.py
+++ sudoku.py
@@ -8,7 +8,30 @@ Created on Sat May  1 11:16:14 2021
     Óscar Esteban Quiroz López
 """
 
+
 with open("sudoku.txt") as sudokuFile:
     arraySudoku = [line.split(" ") for line in sudokuFile]
     
-print(arraySudoku)
+
+# Verifica que todas las filas tengan una longitud de 9
+def verifyColumnLen(arraySudoku):
+    for i in arraySudoku:
+        if(len(i)!=9):
+            return False
+    return True
+
+# Verifica que el sudoku sea de 9x9
+def verifyLenSudoku(arraySudoku):
+    if(len(arraySudoku)==9):
+        if(verifyColumnLen(arraySudoku)):
+            return True
+    return False
+
+# Validación de formato longitud de sudoku
+print(verifyLenSudoku(arraySudoku))
+
+
+    
+
+
+
